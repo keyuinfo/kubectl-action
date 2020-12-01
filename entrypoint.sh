40ae3683c13474e66319d393ad0e9baaec668aa8
Wan Yi <root@wanyi.dev>
2020-12-01 09:31:01 +0800

    添加资源前把成功的任务先删除

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ set -e
 mkdir -p ~/.kube
 echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
 
+set +e
+# 删除所有成功的Jobs
+sh -c "kubectl delete job $(kubectl get job -o=jsonpath='{.items[?(@.status.succeeded==1)].metadata.name}')"
+set -e
+
 # 根据参数Version修改发布文件
 sed -i "s/v1.0/$INPUT_VERSION/g" $*
 
