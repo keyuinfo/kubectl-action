e9c6b41e97c401c4bd145702cbaa5a187a2d94bf
Wan Yi <root@wanyi.dev>
2020-12-01 15:08:27 +0800

    妥善处理已经存在的jobname

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,14 @@ set -e
 mkdir -p ~/.kube
 echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
 
-OUTDATED_JOBS=$(kubectl get job -o=jsonpath='{.items[?(@.status.succeeded==1)].metadata.name}')
-if [ ! -z "$OUTDATED_JOBS" ]
-then
-	# 删除所有成功的Jobs
-	sh -c "kubectl delete job $OUTDATED_JOBS"
-fi
-
 # 根据参数Version修改发布文件
 sed -i "s/v1.0/$INPUT_VERSION/g" $*
 
-sh -c "kubectl apply -f $*"
+pods=$(kubectl get jobs --selector=job-name=$INPUT_JOBNAME --output=jsonpath='{.items[*].metadata.name}')
+if [ ! -z "$pods" ]
+then
+	sh -c "kubectl delete job $pods && kubectl apply -f $*"
+else
+	sh -c "kubectl apply -f $*"
+fi
+
