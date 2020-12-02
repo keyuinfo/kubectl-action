5cc2e91bc4431569ffb7fb75e8a66d5cebdc34e7
Wan Yi <root@wanyi.dev>
2020-12-02 11:01:42 +0800

    完善更新

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,8 @@ echo $INPUT_KUBECONFIG | base64 -d > ~/.kube/config
 # 根据参数Version修改发布文件
 sed -i "s/v1.0/$INPUT_VERSION/g" $*
 
-pods=$(kubectl get jobs --selector=job-name=$INPUT_JOBNAME --output=jsonpath='{.items[*].metadata.name}')
-if [ ! -z "$pods" ]
-then
-	sh -c "kubectl delete job $pods && kubectl apply -f $*"
-else
-	sh -c "kubectl apply -f $*"
-fi
+set +e
+sh -c "kubectl delete job -n $INPUT_NAMESPACE $INPUT_JOBNAME"
+set -e
 
+sh -c "kubectl apply -n $INPUT_NAMESPACE -f $*"
